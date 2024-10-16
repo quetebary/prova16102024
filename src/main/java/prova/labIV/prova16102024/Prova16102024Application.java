f2d6053c9475e2cb373160aca4cbf10234541477
Henrique de Castro Silva <fatec.henrique@gmail.com>
2024-10-16 11:09:08 -0300

    Initial commit

---
+++ src/main/java/prova/labIV/prova16102024/Prova16102024Application.java
@@ -0,0 +1,13 @@
+package prova.labIV.prova16102024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova16102024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova16102024Application.class, args);
+	}
+
+}
---
+++ src/test/java/prova/labIV/prova16102024/Prova16102024ApplicationTests.java
@@ -0,0 +1,13 @@
+package prova.labIV.prova16102024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova16102024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
